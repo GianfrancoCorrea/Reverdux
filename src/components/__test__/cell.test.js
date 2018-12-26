879a40bd8d65293a038d86517542e0d145b74dd8
slyhunt <slyhunt13@gmail.com>
2018-12-26 17:01:12 -0300

    Jest + Enzyme configuration

---
+++ src/components/__test__/cell.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import cell from '../cell';
+
+
+let wrapper  =  2;
+
+describe('Cell Component Test Suite', () => {
+	test('First test', () => {
+		expect(wrapper).toBe(2);
+	})
+})
